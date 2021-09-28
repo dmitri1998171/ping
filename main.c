34418e94e09bb5adac8220bb662d798d434e60b2
dmitri1998171 <dmitri199817@gmail.com>
2021-09-28 21:14:02 +0700

    add send a group of icmp-packages

--- main.c
+++ main.c
@@ -24,12 +24,12 @@ void DieWithError(char *str, int errorCode) {
     exit(1);
 }
 
-long mtime() {
-  struct timeval t;
+float mtime() {
+    struct timeval t;
 
-  gettimeofday(&t, NULL);
-  long mt = (long)t.tv_sec * 1000 + t.tv_usec / 1000;
-  return mt;
+    gettimeofday(&t, NULL);
+    float mt = (float)t.tv_usec / 1000 ;
+    return mt;
 }
 
 unsigned short in_cksum(unsigned short *addr, int len) {
@@ -63,15 +63,15 @@ unsigned short in_cksum(unsigned short *addr, int len) {
 int main(int argc, char **argv) {
     char* servIP = "8.8.8.8";
     int sock;
+    int seq = 1;
     int timeout = 1000;
     int timeoutCounter = 0;
     pid_t pid = getpid();
-    long _time;
-    char data[64];
+    float _time;
     char echoBuffer[RCVBUFSIZE];
     struct sockaddr_in echoServAddr;
     struct icmp_struct icmp, *recv_icmp;
-
+    
     if(argc == 2) servIP = argv[1];
     if(argc == 3) {
         servIP = argv[1];
@@ -90,42 +90,35 @@ int main(int argc, char **argv) {
     echoServAddr.sin_family = AF_INET;
     echoServAddr.sin_addr.s_addr = inet_addr(servIP);
     
-    icmp.type = 8;      // 8 - запрос, 0 - ответ
-    icmp.code = 0;
-    icmp.id = pid;
-    icmp.seq = 1;
-
-    _time = mtime();
-    sprintf(icmp.data, "%ld", _time);
-
-    icmp.checksum = in_cksum((u_short *) &icmp, sizeof(icmp));
-    
-    int sentBytes, recvBytes = 0;
-
     while(1) {
-        if(! (sentBytes = sendto(sock, &icmp, sizeof(icmp), 0, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr))))
-            DieWithError("send() sent a different number of bytes than expected", 1);
+        icmp.type = 8;          // 8 - запрос, 0 - ответ
+        icmp.code = 0;
+        icmp.id = pid;
+        icmp.seq = seq;
+        icmp.checksum = 0;
+        
+        _time = mtime();
+        sprintf(icmp.data, "%f", _time);
 
-        printf("Sent %i bytes\n", sentBytes);
+        icmp.checksum = in_cksum((u_short *) &icmp, sizeof(icmp));
+
+        if(!sendto(sock, &icmp, sizeof(icmp), 0, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)))
+            DieWithError("send() sent a different number of bytes than expected", 1);
 
         // timeoutCounter++;
         // if(timeoutCounter > timeout) {
-            recvBytes = recvfrom(sock, echoBuffer, RCVBUFSIZE, 0, 0, 0);
-            printf("Receive %i bytes\n", recvBytes);
+        recvfrom(sock, echoBuffer, RCVBUFSIZE, 0, 0, 0);
+        recv_icmp = (struct icmp_struct *)(echoBuffer + 20);
 
-            recv_icmp = &echoBuffer[20];
-            // printf("\ntype = %hu\n", recv_icmp->type);
-            // printf("code = %hu\n", recv_icmp->code);
-            // printf("id = %hu\t pid = %hu\n", recv_icmp->id, icmp.id);
-            // printf("seq = %hu\n\n", recv_icmp->seq);
-
-        if(recv_icmp->type == 0) {// && recv_icmp->id == pid) {
-        //     printf("recv packet with id = %hu\n", recv_icmp->id);
+        if(recv_icmp->type == 0 && recv_icmp->id == pid + 8) {
             _time = mtime() - _time;
-            printf("seq= %i timeout= %i time= %ld ms\n", recv_icmp->seq, timeout, _time);
+            
+            if(_time > 0)
+                printf("seq = %i timeout = %i time = %.3f ms\n", recv_icmp->seq, timeout, _time);
         }
         
-        sleep(timeout / 1000);
+        seq++;
+        usleep(timeout * 1000);
     }
 
     return 0;
